addaab0be0a771cd497f607c04daa838a89e9d81
Daniel <daniel.maansson@gmail.com>
2014-02-27 16:52:16 +0100

    Added placeholder POST

---
+++ webapp/js/request.js
@@ -0,0 +1,15 @@
+function getAllData(callback) {
+	
+	$.ajax({
+	    type: "GET",
+	    url: "test",
+	    contentType: "application/json; charset=utf-8",
+	    dataType: "json",
+	    success: function(data) {
+	    	callback(true, data);
+	    },
+	    failure: function(errMsg) {
+	    	callback(false, errMsg);
+	    }
+	});
+}
