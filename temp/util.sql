7ae385e1f49471af95355baee4bd8ef3e666e3e2
tereseno <tereseno@MAGENTA02.ug.kth.se>
2014-02-28 12:10:57 +0100

    Changing to correct Packet.TypeId

--- temp/util.sql
+++ temp/util.sql
@@ -18,17 +18,17 @@ SELECT
 	TimePeriod,
 	HitCount, 
 	Protocols.Name AS Protocol,
-        Types.Name AS TypeName,
-        FromAddr.Addr AS FromAddr,
-        FromLoc.Country AS FromCountry,
-        FromLoc.City AS FromCity,
-        FromLoc.Latitude AS FromLatitude,
-        FromLoc.Longitude AS FromLongitude,
-        ToAddr.Addr AS ToAddr ,
-        ToLoc.Country AS ToCountry,
-        ToLoc.City AS ToCity,
-        ToLoc.Latitude AS ToLatitude,
-        ToLoc.Longitude AS ToLongitude
+	Types.Name AS TypeName,
+	FromAddr.Addr AS FromAddr,
+	FromLoc.Country AS FromCountry,
+	FromLoc.City AS FromCity,
+	FromLoc.Latitude AS FromLatitude,
+	FromLoc.Longitude AS FromLongitude,
+	ToAddr.Addr AS ToAddr,
+	ToLoc.Country AS ToCountry,
+	ToLoc.City AS ToCity,
+	ToLoc.Latitude AS ToLatitude,
+	ToLoc.Longitude AS ToLongitude
 
 FROM Packets
 
@@ -36,7 +36,7 @@ INNER JOIN Protocols
 	ON Packets.ProtocolId = Protocols.ProtocolId
 
 INNER JOIN Types
-        ON Types.TypeId = Packets.PacketId
+        ON Types.TypeId = Packets.TypeId
 
 INNER JOIN Connections
 	ON Packets.ConnectionId = Connections.ConnectionId
@@ -51,4 +51,6 @@ INNER JOIN Addresses AS ToAddr
 	ON Connections.ToAddr = ToAddr.AddrId
 
 INNER JOIN Locations AS ToLoc
-       ON ToLoc.LocationId = ToAddr.AddrId;
+       ON ToLoc.LocationId = ToAddr.AddrId
+
+ORDER BY Time;
