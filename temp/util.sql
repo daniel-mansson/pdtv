71b2dcb95eac235785f09cec8de29a5ae43801c3
tereseno <tereseno@MAGENTA02.ug.kth.se>
2014-02-28 11:31:14 +0100

    Adding things to select from database

--- temp/util.sql
+++ temp/util.sql
@@ -17,14 +17,38 @@ SELECT
 	Time,
 	TimePeriod,
 	HitCount, 
-	Protocols.Name,
-	ToAddr.Addr
+	Protocols.Name AS Protocol,
+        Types.Name AS TypeName,
+        FromAddr.Addr AS FromAddr,
+        FromLoc.Country AS FromCountry,
+        FromLoc.City AS FromCity,
+        FromLoc.Latitude AS FromLatitude,
+        FromLoc.Longitude AS FromLongitude,
+        ToAddr.Addr AS ToAddr ,
+        ToLoc.Country AS ToCountry,
+        ToLoc.City AS ToCity,
+        ToLoc.Latitude AS ToLatitude,
+        ToLoc.Longitude AS ToLongitude
+
 FROM Packets
+
 INNER JOIN Protocols
 	ON Packets.ProtocolId = Protocols.ProtocolId
+
+INNER JOIN Types
+        ON Types.TypeId = Packets.PacketId
+
 INNER JOIN Connections
 	ON Packets.ConnectionId = Connections.ConnectionId
+
 INNER JOIN Addresses AS FromAddr
 	ON Connections.FromAddr = FromAddr.AddrId
+
+INNER JOIN Locations AS FromLoc
+        ON FromLoc.LocationId = FromAddr.LocationId
+
 INNER JOIN Addresses AS ToAddr
-	ON Connections.ToAddr = ToAddr.AddrId;
+	ON Connections.ToAddr = ToAddr.AddrId
+
+INNER JOIN Locations AS ToLoc
+       ON ToLoc.LocationId = ToAddr.AddrId;
