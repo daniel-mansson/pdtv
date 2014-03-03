99379db748817573e81bc95ecdfd6e79affa8d7a
Jussi <jussis@kth.se>
2014-03-03 17:41:43 +0100

    Fixat med slidern

--- webapp/js/Slider.js
+++ webapp/js/Slider.js
@@ -4,7 +4,7 @@ function Slider(model) {
 
 	//this.update(this.model);
 	
-	this.minTime = Date.parse("2014-01-01");
+	this.minTime = Date.parse("2014-01-20");
 	this.maxTime = new Date().getTime();
 	this.rangeSlider = $("#slider").slider({
 		range:true,
@@ -12,10 +12,28 @@ function Slider(model) {
 		max: this.maxTime,
 		values: [this.minTime,this.maxTime]
 	});
+	$("#sliderContainer").width($(window).width()*0.85).height(60).position({
+		my:"center bottom",
+		at:"center bottom-10",
+		of: window
+	});
 	this.rangeSlider.width($(window).width()*0.8).position({
 		my:"center center",
-		at:"center bottom-50",
-		of: window
+		at:"center bottom-30",
+		of: "#sliderContainer"
+	});
+	$('#min').html("2014-01-20").position({
+	    my: 'center bottom',
+	    at: 'center top',
+	    of: $('#slider a:eq(0)'),
+	    offset: "0, 10"
+	});
+
+	$('#max').html(new Date(this.rangeSlider.slider('values', 1)).toISOString().substring(0, 10)).position({
+	    my: 'center top',
+	    at: 'center bottom',
+	    of: $('#slider a:eq(1)'),
+	    offset: "0, 10"
 	});
 }
 
@@ -25,8 +43,29 @@ Slider.prototype.update = function(model) {
 	if(model.data.data.length == 0)
 		return;
 	
+	
+	this.rangeSlider.slider({
+		slide:function(event,ui) {
+	        var delay = function() {
+	            var handleIndex = $(ui.handle).data('uiSliderHandleIndex');
+	            var date= new Date(ui.values[handleIndex]).toISOString().substring(0, 10);
+	            var label = handleIndex == 0 ? '#min' : '#max';
+	            var myPos = handleIndex == 0 ? 'center bottom' : 'center top';
+	            var atPos = handleIndex == 0 ? 'center top' : 'center bottom';
+	            $(label).html(date).position({
+	                my: myPos,
+	                at: atPos,
+	                of: ui.handle,
+	                offset: "0, 10"
+	            });
+	        };
+
+	        // wait for the ui.handle to set its position
+	        setTimeout(delay, 5);
+		}
+	});
 	//TODO: make sure this actually works. Everything might not need to be updated when the model changes.
-	//var model = this.model;
+	var model = this.model;
 	this.rangeSlider.slider({
 		change:function(event,input) {
 			console.log("Slider change");
